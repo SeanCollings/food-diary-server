b042b191ea9557fe110a29f83d0562aff1319e31
Sean Collings <nightharrier@gmail.com>
2022-12-01 18:43:29 +0200

    link all modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { DiaryModule } from '@/diary/diary.module';
+import { UsersModule } from '@/users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MealsModule } from './meals/meals.module';
+import { WellnessModule } from './wellness/wellness.module';
+import { TrendsModule } from './trends/trends.module';
+import { ShareModule } from './share/share.module';
+import { SummaryModule } from './summary/summary.module';
 
 @Module({
-  imports: [DiaryModule],
+  imports: [
+    UsersModule,
+    DiaryModule,
+    AuthModule,
+    MealsModule,
+    WellnessModule,
+    TrendsModule,
+    ShareModule,
+    SummaryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
