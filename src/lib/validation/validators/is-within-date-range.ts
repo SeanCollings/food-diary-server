ac1ab0f1b9c576f83ebc8f63550e762e8998f6a3
Sean Collings <nightharrier@gmail.com>
2022-12-02 15:01:26 +0200

    update validator to use server date

--- src/lib/validation/validators/is-within-date-range.ts
+++ src/lib/validation/validators/is-within-date-range.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { MAX_SUMMARY_MONTH_RANGE } from '@/lib/constants/validation/validation.constants';
-import { getDateMonthsAgo } from '@/utils/date-utils';
+import { formatToServerDate, getDateMonthsAgo } from '@/utils/date-utils';
 import {
   registerDecorator,
   ValidationOptions,
@@ -25,12 +25,11 @@ export function IsWithinDateRange(
             relatedPropertyName
           ] as string;
 
-          const earliestAllowedDate = getDateMonthsAgo(
-            value,
-            MAX_SUMMARY_MONTH_RANGE,
+          const earliestAllowedDate = formatToServerDate(
+            getDateMonthsAgo(value, MAX_SUMMARY_MONTH_RANGE),
           );
 
-          return new Date(relatedValue) >= earliestAllowedDate;
+          return new Date(relatedValue) >= new Date(earliestAllowedDate);
         },
       },
     });
