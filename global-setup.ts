b4e13618c074f97d42216540206c8c0b44d2bf2c
Sean Collings <nightharrier@gmail.com>
2023-05-29 20:23:33 +0200

    update test tz to utc

--- global-setup.ts
+++ global-setup.ts
@@ -1,5 +1,5 @@
 export default () => {
-  process.env.TZ = 'Africa/Johannesburg';
+  process.env.TZ = 'UTC';
   process.env.DATABASE_URL = 'postgresql://';
   process.env.JWT_SECRET = 'test_secret';
 };
