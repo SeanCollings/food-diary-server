0fe360e6b45509c52025a5289c8a8ae82060d106
Sean Collings <nightharrier@gmail.com>
2023-07-25 10:15:49 +0200

    add node-prune

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors();
-  // app.use(helmet());
+  app.enableCors();
+  app.use(helmet());
 
   const port = process.env.PORT || 3000;
 
