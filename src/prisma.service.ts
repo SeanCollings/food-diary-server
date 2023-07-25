aa15da344472b5fee674765b50e0eabe05e1312b
Sean Collings <nightharrier@gmail.com>
2023-07-25 21:33:16 +0200

    remove all code from prisma onmoduleinit

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -4,11 +4,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    try {
-      // Note: this is optional
-      // await this.$connect();
-    } catch (err) {
-      console.error('[PrismaService: $connect] ::', err.message);
-    }
+    // try {
+    //   // Note: this is optional
+    //   // await this.$connect();
+    // } catch (err) {
+    //   console.error('[PrismaService: $connect] ::', err.message);
+    // }
   }
 }
