4b4f62b4b1648b3b922a92fe43307eb2cd7f285e
Sean Collings <nightharrier@gmail.com>
2023-07-25 21:25:08 +0200

    remove primsma connect

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,7 +5,8 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     try {
-      await this.$connect();
+      // Note: this is optional
+      // await this.$connect();
     } catch (err) {
       console.error('[PrismaService: $connect] ::', err.message);
     }
